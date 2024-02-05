8492536ea6e13718334f3750c63924975f31f7b1
Alyona <besssta888@mail.ru>
2024-02-05 14:17:37 +0300

    refactor: refactor showGreeting.js

--- src/functions/showMessage/showGreeting.js
+++ src/functions/showMessage/showGreeting.js
@@ -4,5 +4,5 @@ const GREETING_TEMPLATE_PART1 = 'Welcome to the File Manager, ';
 const GREETING_TEMPLATE_PART2 = '!';
 
 export const showGreeting = () => {
-  console.log(GREETING_TEMPLATE + defineUsername() + GREETING_TEMPLATE_PART2);
+  console.log(GREETING_TEMPLATE_PART1 + defineUsername() + GREETING_TEMPLATE_PART2);
 };
